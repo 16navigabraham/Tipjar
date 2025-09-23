fe8b939659fc9014271697ec1fd2a6f4af7fc199
Adebanjo Abraham <navigatorabraham17@gmail.com>
2025-09-23 20:05:03 +0000

    Error: Project ID is not defined     at [project]/src/lib/config.ts [app

--- next.config.ts
+++ next.config.ts
@@ -24,6 +24,9 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  env: {
+    NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID,
+  },
 };
 
 export default nextConfig;
