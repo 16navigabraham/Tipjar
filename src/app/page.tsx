30de88ccb88a90daecc1b9a78080d708a56cf5e9
Adebanjo Abraham <navigatorabraham17@gmail.com>
2025-09-23 22:40:19 +0000

    remove this from the landing page : Find a Creator Enter a creator's use

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import { Header } from '@/components/layout/header';
-import { CreatorLookup } from '@/components/creator-lookup';
+import { TipJarCard } from '@/components/tip-jar-card';
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen bg-background">
       <Header />
       <main className="flex-1 flex items-center justify-center p-4 sm:p-6">
-        <CreatorLookup />
+        <TipJarCard />
       </main>
     </div>
   );
