5c3237f4a2084cdc57a985c69ee6c1b211508380
Adebanjo Abraham <navigatorabraham17@gmail.com>
2025-09-26 16:29:13 +0000

    use the abi to test , to correct tip txn properly

--- src/services/contractService.ts
+++
@@ -1,7 +0,0 @@
-'use server';
-import { ethers } from 'ethers';
-import { db } from '@/lib/firebase';
-import { collection, addDoc } from 'firebase/firestore';
-
-// This file is now unused and can be deleted. The logic has been moved to the use-tip-contract hook.
-// Keeping it for now to avoid breaking imports, but it should be removed in a future refactor.
