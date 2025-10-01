2c5b0a5d098ed9a34081895be8cd20ea8b64a4b2
Adebanjo Abraham <navigatorabraham17@gmail.com>
2025-10-01 17:01:34 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -3,19 +3,6 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Cross-Origin-Opener-Policy',
-            value: 'same-origin-allow-popups',
-          },
-        ],
-      },
-    ];
-  },
   images: {
     remotePatterns: [
       {
