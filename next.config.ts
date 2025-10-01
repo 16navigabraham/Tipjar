a75a57b99e95578feb9a7a4d893abe68f5e0bf8e
Adebanjo Abraham <navigatorabraham17@gmail.com>
2025-10-01 16:30:51 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -48,11 +48,6 @@ const nextConfig: NextConfig = {
     NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID,
     NEXT_PUBLIC_ALCHEMY_BASE_URL: process.env.NEXT_PUBLIC_ALCHEMY_BASE_URL,
   },
-  webpack: (config) => {
-    config.externals.push('pino-pretty', 'lokijs', 'encoding');
-    config.resolve.alias['@react-native-async-storage/async-storage'] = false;
-    return config;
-  },
 };
 
 export default nextConfig;
